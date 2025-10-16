9696bc95f30e58bf47ab2748dd4e329351553982
Niklas <niklasbeck18@gmail.com>
2025-10-16 16:27:30 +0200

    Navbar responsif fix

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,8 @@
 const Navbar = () => {
     return (
         <nav className="w-full sticky top-0 z-50 backdrop-blur-xl">
-            <div className="relative py-3">
-                <ul className="flex justify-center gap-6 text-xl text-white">
+            <div className="relative py-2 sm:py-3">
+                <ul className="flex justify-center gap-3 sm:gap-4 md:gap-6 text-sm sm:text-base md:text-xl text-white px-4">
                     <li><a href="/#home" className="hover:opacity-90">Home</a></li>
                     <li><a href="/#about" className="hover:opacity-90">Über mich</a></li>
                     <li><a href="/#projects" className="hover:opacity-90">Projekte</a></li>
